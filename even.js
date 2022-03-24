9a4b483eb6c0c42359a7b8ee278f40d82c23c96a
Thushara <thusharan@hotmail.com>
2022-03-23 22:05:24 -0400

    console.log with diff function varieties

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+// so basically if num is even, it will return as 0 which is true.
+// if it was 1, it would be false
+ //const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+//you don't need to set our fcn return values to new variables 
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+console.log(isEven(10));
+console.log(isEven(11));
