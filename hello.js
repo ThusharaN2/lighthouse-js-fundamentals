62f72c2a7e714ccb79619d46ba3f1f5085df0171
Thushara <thusharan@hotmail.com>
2022-03-23 21:56:31 -0400

    added return and console examples

--- hello.js
+++ hello.js
@@ -10,3 +10,16 @@
 //sayHello("Miranda");
 //sayHello("Ferdinand");
 
+
+// const sayHelloToConsole = function (name) {
+// console.log("Hello, " + name);
+//}
+//sayHelloToConsole("John");
+
+//const returnSayHello = function (name) {
+//  return "Hello, " + name;
+//}
+//const greeting = returnSayHello("John");
+//console.log(greeting);
+
+
